8582d1fec588190649c2c4d1d6b785283ca334bf
SmartBuoy-eng <81980944+SmartBuoy-eng@users.noreply.github.com>
2021-05-11 00:25:58 -0400

    Add files via upload

---
+++ SB_tester.ino
@@ -0,0 +1,75 @@
+#include "arduino_secrets.h"
+#include "secrets.h"
+#include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
+
+char ssid[] = SECRET_SSID;   // your network SSID (name) 
+char pass[] = SECRET_PASS;   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+WiFiClient  client;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+
+// Initialize our values
+int temp = 0;
+int sc = 0;
+int dox = 0;
+int ph = 0;
+
+void setup() {
+  Serial.begin(115200);  // Initialize serial
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo native USB port only
+  }
+  
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv != "1.0.0") {
+    Serial.println("Please upgrade the firmware");
+  }
+    
+  ThingSpeak.begin(client);  //Initialize ThingSpeak
+}
+
+void loop() {
+
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass); // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected.");
+  }
+
+  // set the fields with the values
+  ThingSpeak.setField(1, temp);
+  ThingSpeak.setField(2, sc);
+  ThingSpeak.setField(3, dox);
+  ThingSpeak.setField(4, ph);
+
+  // write to the ThingSpeak channel
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if(x == 200){
+    Serial.println("Channel update successful.");
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+  
+  // change the values
+  temp = newTemp;
+  sc = newSc;
+  dox = newDox;
+  pH = newPh;
+  
+  delay(20000); // Wait 20 seconds to update the channel again
---
+++ arduino_secrets.h
@@ -0,0 +1,4 @@
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+#define SECRET_CH_ID ""
+#define SECRET_WRITE_APIKEY ""
